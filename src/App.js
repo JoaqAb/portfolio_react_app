07d51133916e713f1b2d7f1df3fa853f657459a5
JoaqAb <joaquinabuin@gmail.com>
2023-07-05 16:19:10 -0300

    cambio de nombre Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Home from "./components/Home";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import Works from "./components/Works";
 
